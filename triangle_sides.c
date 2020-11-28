94d22ead6d54a4aa4e0ba68d74f84a4faa786a71
r0ss1n1 <43396895+r0ss1n1@users.noreply.github.com>
2020-11-28 03:36:07 -0800

    Add files via upload

---
+++ triangle_sides.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#define RADIANS_TO_DEGREES 180 / 3.14159265358979323846
+
+/* I love you family, Tai, Mark, Kerri & June */
+/* Charles Thomas Wallace Truscott */
+typedef struct Triangle {
+	float opposite;
+	float adjacent;
+	float hypotenuse;
+
+} Triangle;
+
+int main(void) {
+	Triangle FS;
+	printf("Authored by Charles Thomas Wallace Truscott, Suffolk Park / Byron Bay NSW 2481 Australia\n\n");
+	printf("Vale Herbert Wallace Truscott\tVale Thomas Watters, Lillian May Turner\n\n");
+	printf("Enter the length of the opposite side:\t");
+	scanf("%f", &FS.opposite);
+	printf("\n\n");
+	printf("Enter the length of the adjacent side:\t");
+	scanf("%f", &FS.adjacent);
+	printf("\n\n");
+	printf("Enter the length of the hypotenuse:\t");
+	scanf("%f", &FS.hypotenuse);
+	printf("\n\n");
+	Triangle * S = &FS;
+	printf("Opposite / Hypotenuse: %f\nAdjacent / Hypotenuse: %f\nOpposite / Adjacent:   %f\n\n", S->opposite / S-> adjacent, S->adjacent / S->hypotenuse, S->opposite / S->adjacent);
+	printf("\n\n");
+	printf("Opposite / Hypotenuse Angle: %f\nAdjacent / Hypotenuse Angle: %f\nOpposite / Adjacent Angle:   %f\n\n", asin(S->opposite / S-> adjacent) * RADIANS_TO_DEGREES, acos(S->adjacent / S->hypotenuse) * RADIANS_TO_DEGREES, atan(S->opposite / S->adjacent) * RADIANS_TO_DEGREES);
+	return 0;
+}
